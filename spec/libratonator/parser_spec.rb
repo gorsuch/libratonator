1850ae518fa8f2ad0b4eb947430d4f9fc6a1ad4a
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-09-26 13:04:25 -0500

    whitespace

--- spec/libratonator/parser_spec.rb
+++ spec/libratonator/parser_spec.rb
@@ -4,6 +4,7 @@ describe Libratonator::Parser do
   describe '#parse' do
     let(:parser) { Libratonator::Parser.new }
     let(:sample) { 'foo 12345 67890' }
+
     it 'should return an array' do
       parser.parse(sample).should be_kind_of(Array)
     end
