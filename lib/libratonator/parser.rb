acd68b5fb11de63635f46bb816b5a2f08c480082
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-09-26 13:21:02 -0500

    parser specs passing

--- lib/libratonator/parser.rb
+++ lib/libratonator/parser.rb
@@ -1,7 +1,8 @@
 module Libratonator
   class Parser
     def parse(s)
-      {}
+      parts = s.split(' ')
+      {parts[0] => {'value' => parts[1], 'measure_time' => parts[2]}}
     end
   end
 end
--- spec/libratonator/parser_spec.rb
+++ spec/libratonator/parser_spec.rb
@@ -8,5 +8,23 @@ describe Libratonator::Parser do
     it 'should return a hash' do
       parser.parse(sample).should be_kind_of(Hash)
     end
+
+    it 'should return a hash with the metric name as the key' do
+      parser.parse(sample).keys.first.should eq('foo')
+    end
+
+    it 'should return a hash with only one key' do
+      parser.parse(sample).keys.size.should eq(1)
+    end
+
+    it 'should give a hash that includes the value' do
+      parser.parse(sample)['foo'].should have_key('value')
+      parser.parse(sample)['foo']['value'].should eq('12345')
+    end
+
+    it 'should give a hash that includes the measure_time' do
+      parser.parse(sample)['foo'].should have_key('measure_time')
+      parser.parse(sample)['foo']['measure_time'].should eq('67890')
+    end
   end
 end
